f2d324b4a8ad3bd139b1fc9f6e1567b75915642f
divyrajgida1232 <93469063+divyrajgida1232@users.noreply.github.com>
2021-11-01 12:04:18 +0530

    initial commit

---
+++ main/java/com/blackd/tiptime/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.blackd.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.blackd.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import java.text.NumberFormat.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+
+        val tipPercentage = when (selectedId){
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost!!
+        val roundUp = binding.roundUpSwitch.isChecked
+
+        if (roundUp){
+            tip = kotlin.math.ceil(tip)
+        }
+        getCurrencyInstance()
+
+        val formattedTip = getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+}
